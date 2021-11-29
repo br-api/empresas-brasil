918fc5379c1dc2b19b096899de7bb0f211516bb9
br-api <95198310+br-api@users.noreply.github.com>
2021-11-29 02:10:34 -0300

    Create __version__.py

---
+++ empresas_brasil/__version__.py
@@ -0,0 +1,9 @@
+"""Metadata for the empresas_brasil package."""
+
+__title__ = "empresas_brasil"
+__description__ = "Brazilian Company API client"
+__version__ = "0.0.1"
+__author__ = "BR-API Team"
+__author_email__ = "brazilianapi@gmail.com"
+__license__ = " LGPL 3.0"
+__url__ = "https://rapidapi.com/brapi/api/empresas"
